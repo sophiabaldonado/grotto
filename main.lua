e8697e43d9fd386fcabfad849a268eacf0c6f223
sophia <sophiabaldonado@gmail.com>
2020-09-03 01:01:11 -0700

    start swimming;

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
 cave = require 'cave'
+io.stdout:setvbuf('no')
 
 function lovr.load()
   cave:init()
   models = {}
+  world = { x = 0, y = 0, z = 0 }
+  last = { x = 0, y = 0, z = 0 }
 end
 
 function lovr.update(dt)
@@ -10,16 +13,38 @@ function lovr.update(dt)
 end
 
 function lovr.draw()
+  lovr.graphics.push()
+  lovr.graphics.translate(world.x, world.y, world.z)
   cave:draw()
-    -- lovr.graphics.box('line', 0, 0, 0, .1, .1, .1)
+  lovr.graphics.pop()
     
   for i, hand in ipairs(lovr.headset.getHands()) do
     models[hand] = models[hand] or lovr.headset.newModel(hand)
     
     if models[hand] then
       local x, y, z, angle, ax, ay, az = lovr.headset.getPose(hand)
-      -- models[hand]:draw(x, y, z, 1, angle, ax, ay, az)
-      lovr.graphics.box('line', x, y, z, .1, .1, .1, angle, ax, ay, az)
+      lovr.graphics.setColor(0.85, 0.85, 0.85, 0.5)
+      lovr.graphics.sphere(x, y, z, .025, angle, ax, ay, az)
+      lovr.graphics.setColor(1.0, 1.0, 1.0, 1.0)
+    end
+
+    if (lovr.headset.isDown('left', 'x')) then
+      if (hand == 'hand/left') then
+        move(hand)
+      end
+    else
+      world.x = 0
+      world.y = 0
+      world.z = 0
     end
   end
+end
+
+function move(hand)
+  local x, y, z = lovr.headset.getPose(hand)
+  world.x = world.x - (last.x - x)
+  world.z = world.z - (last.z - z)
+  last.x = x
+  last.y = y
+  last.z = z
 end

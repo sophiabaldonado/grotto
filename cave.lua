f07719c5add879041ca00f109fb818bf2335154a
bjorn <tie.372@gmail.com>
2020-09-03 00:34:58 -0600

    Cave scale;

--- cave.lua
+++ cave.lua
@@ -1,17 +1,22 @@
 local cave = {}
 
+cave.scale = .6
+
 function cave:init()
   self.points = {}
   local data = lovr.filesystem.read('assets/cave.obj')
   for x, y, z in data:gmatch('(-?%d*%.%d+) (-?%d*%.%d+) (-?%d*%.%d+)') do
-    table.insert(self.points, tonumber(x) / 10)
-    table.insert(self.points, tonumber(y) / 10)
-    table.insert(self.points, tonumber(z) / 10)
+    table.insert(self.points, tonumber(x))
+    table.insert(self.points, tonumber(y))
+    table.insert(self.points, tonumber(z))
   end
 end
 
 function cave:draw()
+  lovr.graphics.push()
+  lovr.graphics.scale(self.scale)
   lovr.graphics.points(self.points)
+  lovr.graphics.pop()
 end
 
 return cave

a4583efd288163daca95c0b25e590018925141d0
sophia <sophiabaldonado@gmail.com>
2020-09-06 18:04:32 -0700

    cleanup stuff and add music to forest scene;

--- forest.lua
+++ forest.lua
@@ -1,41 +1,49 @@
-local dome = {}
+local forest = {}
 
-dome.scale = 2
+forest.scale = 2
 
-function dome:init()
+function forest:init()
   local points = {}
   local data = lovr.filesystem.read('assets/stardome.obj')
   for x, y, z in data:gmatch('(-?%d*%.%d+) (-?%d*%.%d+) (-?%d*%.%d+)') do
     table.insert(points, { x, y, z })
   end
 
-  self.mesh = lovr.graphics.newMesh({{ 'lovrPosition', 'float', 3 }}, points, 'points', 'static')
-
-  
+  self.domemesh = lovr.graphics.newMesh({{ 'lovrPosition', 'float', 3 }}, points, 'points', 'static')
   self.treetexture = lovr.graphics.newTexture('assets/forest.png')
   self.treematerial = lovr.graphics.newMaterial(self.treetexture, 0, 0, 0, 1)
+
+  self.music = lovr.audio.newSource('assets/forest.ogg', 'static')
   self.fade = 0
+  self.conclude = false
 end
 
-function dome:update(dt)
+function forest:update(dt)
+  -- placeholder
   for i, hand in ipairs(lovr.headset.getHands()) do
     if (hand == 'hand/right') then
       if (lovr.headset.isDown(hand, 'trigger')) then
-        self.fade = math.min(self.fade + dt, 1)
+        self.conclude = true
       else
-        self.fade = math.max(self.fade - dt, 0)
+        self.conclude = false
+        self.fade = math.max(self.fade - dt * 2, 0)
       end
     end
   end
+
+  if self.conclude then
+    self.music:play()
+    self.fade = math.min(self.fade + dt * 1.5, 1)
+  end
 end
 
-function dome:draw()
+function forest:draw()
   local a = vec3(0, 0, 0)
   local b = vec3(3 / 255, 3 / 255, 10 / 255)
   local c = a:lerp(b, self.fade)
   lovr.graphics.setBackgroundColor(c:unpack())
 
-  self.mesh:draw(0, 0, 0, self.scale)
+  self.domemesh:draw(0, 0, 0, self.scale)
   lovr.graphics.circle('fill', 0, 30, -5, 1, math.pi / 2, 1, 0, 0)
 
   lovr.graphics.setCullingEnabled(true)
@@ -45,4 +53,4 @@ function dome:draw()
   lovr.graphics.setCullingEnabled(false)
 end
 
-return dome
+return forest
--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 cave = require 'cave'
-dome = require 'dome'
+forest = require 'forest'
 io.stdout:setvbuf('no')
 
 function lovr.load()
   lovr.headset.setClipDistance(0.1, 100)
   cave:init()
-  dome:init()
+  forest:init()
   swimspeed = 1.5
   world = { x = 0, y = 0, z = 0 }
   last = { x = 0, z = 0 }
@@ -13,6 +13,7 @@ end
 
 function lovr.update(dt)
   for i, hand in ipairs(lovr.headset.getHands()) do
+    -- make work for both hands
     if (hand == 'hand/left') then
       local x, y, z = lovr.headset.getPose(hand)
       if (lovr.headset.isDown(hand, 'trigger')) then
@@ -24,25 +25,29 @@ function lovr.update(dt)
   end
 
   cave:update(dt)
-  dome:update(dt)
+  forest:update(dt)
 end
 
 function lovr.draw()
   lovr.graphics.push()
   lovr.graphics.translate(world.x, world.y, world.z)
   -- cave:draw()
-  dome:draw()
+  forest:draw()
   lovr.graphics.pop()
 
+  drawHands()
+end
+
+function move(x, z)
+  world.x = world.x - (last.x - x) * swimspeed
+  world.z = world.z - (last.z - z) * swimspeed
+end
+
+function drawHands()
   for i, hand in ipairs(lovr.headset.getHands()) do
     local x, y, z, angle, ax, ay, az = lovr.headset.getPose(hand)
     lovr.graphics.setColor(0.85, 0.85, 0.85, 0.5)
     lovr.graphics.sphere(x, y, z, .025, angle, ax, ay, az)
     lovr.graphics.setColor(1.0, 1.0, 1.0, 1.0)
   end
-end
-
-function move(x, z)
-  world.x = world.x - (last.x - x) * swimspeed
-  world.z = world.z - (last.z - z) * swimspeed
 end

a616292e230e784542024998c37554bb17f1c092
bjorn <tie.372@gmail.com>
2020-09-13 12:11:11 -0700

    Octree tests;

--- cave.lua
+++ cave.lua
@@ -1,5 +1,24 @@
 local cave = {}
 
+local octree = require 'points'
+octree.lookup = {}
+for i = 1, #octree do
+  octree.lookup[i] = octree[octree[i].key]
+end
+
+local function clamp(x, min, max)
+  return math.max(min, math.min(max, x))
+end
+
+local function testSphereBox(aabb, sphere, r2)
+  local minx, maxx, miny, maxy, minz, maxz = unpack(node.aabb)
+  local x = clamp(sphere.x, minx, maxx)
+  local y = clamp(sphere.y, miny, maxy)
+  local z = clamp(sphere.z, minz, maxz)
+  local dx, dy, dz = x - sphere.x, y - sphere.y, z - sphere.z
+  return dx * dx + dy * dy + dz * dz < r2
+end
+
 function cave:init()
   cave.active = false
 
@@ -32,10 +51,28 @@ function cave:update(dt)
   self.feeler:send('hands', hands)
   self.feeler:send('dt', dt)
 
+  local r2 = .3 * .3
+  local rooms = { octree }
+  for i, room in ipairs(rooms) do
+    local function visit(node)
+      if not testSphereBox(node.aabb, hands[1], r2) and not testSphereBox(node.aabb, hands[2], r2) then
+        return
+      end
+
+      local child = key * 8
+      local minx, maxx, miny, maxy, minz, maxz = unpack(aabb)
+      local cx, cy, cz = (minx + maxx) / 2, (miny + maxy) / 2, (minz + maxz) / 2
+    end
+
+    visit(room.nodes[1])
+  end
+
   for i = 0, self.count - 1, 65535 do
     self.feeler:send('offset', i)
     lovr.graphics.compute(self.feeler, math.min(self.count - i, 65535))
   end
+
+  self.shader:send('head', vec3(lovr.headset.getPosition()):sub(world))
 
   -- placeholder for when player escapes the cave
   for i, hand in ipairs(lovr.headset.getHands()) do
@@ -48,9 +85,23 @@ end
 function cave:draw()
   if not self.active then return end
 
+  lovr.graphics.setBlendMode()
   lovr.graphics.setShader(self.shader)
   self.mesh:draw()
   lovr.graphics.setShader()
+
+  --[[local head = vec3(lovr.headset.getPosition()):sub(vec3(world.x, world.y, world.z))
+  for i = 1, #octree do
+    if octree[i].leaf then
+      local minx, maxx, miny, maxy, minz, maxz = unpack(octree[i].aabb)
+      local center = vec3((minx + maxx) / 2, (miny + maxy) / 2, (minz + maxz) / 2)
+      if head:distance(center) < 4 then
+        self.mesh:setDrawRange(octree[i].start, octree[i].count)
+        self.mesh:draw()
+      end
+    end
+  end
+  lovr.graphics.setShader()]]
 end
 
 function cave:start()

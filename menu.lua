94b09f14e74889927332733ed6d0ef951309f600
sophia <sophiabaldonado@gmail.com>
2020-09-15 01:14:50 -0700

    add locomotion instruction to menu;

--- menu.lua
+++ menu.lua
@@ -7,6 +7,7 @@ function menu:init()
   self.logo = lovr.graphics.newMaterial(self.logoTex)
 
   self.logoFade = 0
+  self.locoFade = 0
   self.startFade = 0
 end
 
@@ -14,7 +15,8 @@ function menu:update(dt)
   local time = lovr.timer.getTime()
 
   if time > 5 then self.logoFade = math.min(self.logoFade + dt * .1, 1) end
-  if time > 7 then self.startFade = math.min(self.startFade + dt * .1, 1) end
+  if time > 7 then self.locoFade = math.min(self.locoFade + dt * .1, 1) end
+  if time > 9 then self.startFade = math.min(self.startFade + dt * .1, 1) end
 
   if lovr.headset.isDown('hand/right', 'trigger') and
   lovr.headset.isDown('hand/left', 'trigger') then
@@ -30,8 +32,10 @@ function menu:draw()
   lovr.graphics.setColor(1, 1, 1, self.logoFade)
   lovr.graphics.circle('fill', -.065, 1.5, -1.99, self.dotscale)
   lovr.graphics.plane(self.logo, 0, 1.5, -2, self.logoTex:getWidth() * .002, self.logoTex:getHeight() * .002)
+  lovr.graphics.setColor(1, 1, 1, self.locoFade)
+  lovr.graphics.print('Use Trigger to Move', 0, 1, -2, .05)
   lovr.graphics.setColor(1, 1, 1, self.startFade)
-  lovr.graphics.print('Hold Both Triggers to Start', 0, 1, -2, .05)
+  lovr.graphics.print('Hold Both Triggers to Start', 0, .9, -2, .05)
   lovr.graphics.pop()
 end
 
